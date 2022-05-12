6426cc3fccf9594bec66796e2bb52a50815583d0
Xiaochaoy <chaoshixiaochao@gmail.com>
2022-05-12 17:04:10 +0200

    xcvcxv

--- Winner.gd
+++ Winner.gd
@@ -5,4 +5,10 @@ sync func return_to_lobby():
 
 func _on_Win_timer_timeout():
 	if get_tree().is_network_server():
-		rpc("return_to_lobby")
+		for child in Persistent_nodes.get_children():
+			if child.is_in_group("Enemy"):
+				rpc("borrar",child.name)
+	rpc("return_to_lobby")
+
+sync func borrar(enemigo):
+	Persistent_nodes.get_node(enemigo).queue_free()
