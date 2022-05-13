12d63399ad3365ebda1a9a58e80eac9a8a001312
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-13 19:11:44 +0200

    wef

--- Winner.gd
+++ Winner.gd
@@ -1,14 +1,13 @@
 extends Label
 
 sync func return_to_lobby():
+	for child in Persistent_nodes.get_children():
+		if child.is_in_group("Enemy"):
+			Persistent_nodes.get_node(child.name).queue_free()
 	get_tree().change_scene("res://Network_setup.tscn")
+			
 
 func _on_Win_timer_timeout():
-	if get_tree().is_network_server():
-		for child in Persistent_nodes.get_children():
-			if child.is_in_group("Enemy"):
-				rpc("borrar",child.name)
+	
 	rpc("return_to_lobby")
 
-sync func borrar(enemigo):
-	Persistent_nodes.get_node(enemigo).queue_free()
