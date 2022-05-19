1a8a5e3a368c538d77601355f3f5002d1b29174d
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-19 15:12:01 +0200

    qwert

--- Game_UI.gd
+++ Game_UI.gd
@@ -3,9 +3,14 @@ extends CanvasLayer
 onready var win_timer = $Control/Winner/Win_timer
 onready var winner = $Control/Winner
 
+
+onready var esperaOleada_timer = $OleadasControl/TimerEspera
+onready var oleadasLabel = $OleadasControl/LabelOleadas
+
 func _ready() -> void:
 	# escondemos el mensaje de ganador
 	winner.hide()
+	oleadasLabel.visible = true
 
 # Esto se ejecuta cada frame
 func _process(_delta: float) -> void:
@@ -16,3 +21,6 @@ func _process(_delta: float) -> void:
 		
 		if win_timer.time_left <= 0:
 			win_timer.start()
+
+	if get_tree().is_network_server():
+		
